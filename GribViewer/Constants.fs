63f4c151435ba98da5f7794896aa59cc3a1bddcb
john <johnpalmer.au@gmail.com>
2018-03-30 14:49:33 +1100

    First section Parsing hopefully done enough for now - see more when we do the second section

---
+++ GribViewer/Constants.fs
@@ -0,0 +1,60 @@
+﻿module Constants
+
+let weathercenters =  
+    dict 
+        [
+            (7,"US Weather Service - National Met. Center");
+            (8,"US Weather Service - NWS Telecomms Gateway");
+            (9,"US Weather Service - Field Stations");
+            (34,"Japanese Meteorological Agency – Tokyo");
+            (52,"National Hurricane Center, Miami");
+            (54,"Canadian Meteorological Service – Montreal");
+            (57,"U.S. Air Force - Global Weather Center58US Navy - Fleet Numerical Oceanography Center");
+            (59,"NOAA Forecast Systems Lab, Boulder CO");
+            (74,"U.K. Met Office – Bracknell");
+            (85,"French Weather Service – Toulouse");
+            (97,"European Space Agency (ESA)");
+            (98,"European Center for Medium-Range Weather Forecasts – Reading");
+            (99,"DeBilt, Netherlands");
+        ] 
+let parameters = 
+    let reader = new Microsoft.VisualBasic.FileIO.TextFieldParser("GribDataTypes.csv")
+    reader.SetDelimiters(",")
+    let entryList = new ResizeArray<_>()
+    reader.ReadLine() |> ignore //ignore header line
+    while reader.EndOfData |> not do
+        let fields = reader.ReadFields()
+        let ID = fields.[0] |> int
+        let measurementType = fields.[1]
+        entryList.Add(ID,measurementType)
+    done
+    dict entryList
+
+let ForecastTimeUnit t =
+    match t with
+    |0 -> "minute"
+    |1 -> "hour"
+    |2 -> "day"
+    |3 -> "month"
+    |4 -> "year"
+    |5 -> "decade"
+    |6 -> "normal (30Y)"
+    |7 -> "century"
+    |x when x > 7 && x<254 -> "reserved"
+    |254 -> "second"
+    |_ -> failwithf "Invalid forecast time unit"
+
+let TimeRangeIndicator t = 
+    match t with
+    |0 -> "valid at reference time+P1 (P1>0),analysis for reference time (P1=0), image for refernce time (P1=0)"
+    |1 -> "initialized analysis for reference time"
+    |2 -> "Valid between reference time + P1 and reference time + P2"
+    |3 -> "Average (reference time + P1, reference time + P2"
+    |4 -> "Accumulation (reference time + P1 to reference time + P2)"
+    |5 -> "Difference (reference time + P2 minus reference time + P1)"
+    |6|7|8|9 -> "Reserved"
+    |10 -> "P1 is octets 19 and 20, valid at reference time + P1"
+    |x when x>10 && x < 51 -> "Reserved"
+    |51 -> "Mean value - methodology is complex - fill in later"
+    //some other formats, not bothering to explain for now
+    |_ -> failwithf "Some more complicated formats of reference data - not bothering for now"
--- GribViewer/Program.fs
+++ GribViewer/Program.fs
@@ -1,31 +1,16 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open Constants
+// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 let file = "C:\Users\john\Desktop\GRIB Data\Pacific.wind.7days.grb\Pacific.wind.7days.grb"
 
-let weathercenters =  
-    dict 
-        [
-            (7,"US Weather Service - National Met. Center");
-            (8,"US Weather Service - NWS Telecomms Gateway");
-            (9,"US Weather Service - Field Stations");
-            (34,"Japanese Meteorological Agency – Tokyo");
-            (52,"National Hurricane Center, Miami");
-            (54,"Canadian Meteorological Service – Montreal");
-            (57,"U.S. Air Force - Global Weather Center58US Navy - Fleet Numerical Oceanography Center");
-            (59,"NOAA Forecast Systems Lab, Boulder CO");
-            (74,"U.K. Met Office – Bracknell");
-            (85,"French Weather Service – Toulouse");
-            (97,"European Space Agency (ESA)");
-            (98,"European Center for Medium-Range Weather Forecasts – Reading");
-            (99,"DeBilt, Netherlands");
-        ] 
-    
 let getlen (t:byte[]) =
     let intarr = t |> Array.map (int)
     intarr.[0]*256*256 + intarr.[1]*256 + intarr.[2]
 
 //section one is the product definition section
 let readSectionOne (t:System.IO.Stream) =
+    let secOneStartPos = t.Position
     let buffer = Array.zeroCreate 3
     t.Read(buffer,0,3) |> ignore //TODO: insert check here
     let sectonelength = getlen buffer
@@ -34,6 +19,58 @@ let readSectionOne (t:System.IO.Stream) =
     printfn "parameter table version is %i" paramtableversion
     let weathercenter = t.ReadByte()
     printfn "Weather center that produced this forecast is %s" (weathercenters.[weathercenter])
+    let methodnumber = t.ReadByte() //can get some info on this, but would need to look it up - on a per country basis
+    let gridID = t.ReadByte()
+    match gridID with
+    |21 | 22 | 23 |24 |25 |26 |61|62|63|64 -> printfn "IEG grid"
+    |37|38|39|40|41|42|43|44 -> printfn "thinned grid"
+    |255 -> printfn "file will define grid"
+    |_ -> printfn "Unknown grid type"
+    let GDSorMDSPresence = t.ReadByte() <> 0
+    printfn "GDS/MSD is present %b" GDSorMDSPresence
+    let parameterType = parameters.[t.ReadByte()]
+    printfn "Parameter is %s" parameterType
+    //next 3 byets are information about heights for the measurement - ignore for now
+    t.ReadByte() |> ignore
+    t.ReadByte() |> ignore
+    t.ReadByte() |> ignore
+    //next is date of the forecast
+    let yearofcentury = t.ReadByte()
+    let monthOfYear = t.ReadByte()
+    let dayofMonth = t.ReadByte()
+    let hourofDay = t.ReadByte()
+    let minuteofHour = t.ReadByte()
+    let ForecastDate = new DateTime(yearofcentury+2000,monthOfYear,dayofMonth,hourofDay,minuteofHour,0,DateTimeKind.Utc)
+    printfn "Forecast date is %O UTC" ForecastDate
+    //some information now about when the information is for.  It looks like it is even possible for a single entry to contain multiple forecasts
+    //for example, wind every 6 hours is possible
+    let forecastTimeUnit = t.ReadByte() |> ForecastTimeUnit
+    let numberofUnits = t.ReadByte()
+    printfn "Forecast time unit is %i %s"  numberofUnits forecastTimeUnit
+    let TimeBetweenForecasts = t.ReadByte()
+    printfn "Time between forecasts is %i %s" TimeBetweenForecasts forecastTimeUnit
+    let timeRangeIndicator = t.ReadByte() |> TimeRangeIndicator
+    printfn "Forecast is %s" timeRangeIndicator
+    let zbyte = t.ReadByte()
+    if zbyte <> 0 then failwithf "Expected 0 (this means an averaging forecast which I don't know about)"
+    let zbyte2 = t.ReadByte()
+    if zbyte2 <> 0 then failwithf "Expected 0 (this means an averaging forecast which I don't know about)"
+    let zbyte3 = t.ReadByte()
+    if zbyte3 <> 0 then failwithf "Expected 0 (this is the number missing from an average)"
+    let century = t.ReadByte()
+    if century <> 21 then failwithf "Only doing forecasts in the 2000's for now"
+    let sub_center = t.ReadByte() //don't know anything about this yet
+    let mutable scalebit1 = t.ReadByte()
+    if scalebit1 > 128 then
+        scalebit1 <- -(scalebit1-128)
+    let scalebit2 = t.ReadByte()
+    let D = scalebit1*256 + scalebit2
+    printfn "D value is %i" D
+    let dummybytes = (secOneStartPos + (sectonelength |> int64) - t.Position) |> int
+    let emptybuffer = Array.zeroCreate dummybytes
+    t.Read(emptybuffer,0,dummybytes) |> ignore
+    printfn "Finished reading section 1"
+    ()
 let readHeader (t:System.IO.Stream) =
     let buffer = Array.zeroCreate 8
     let checklen = t.Read(buffer,0,8)
