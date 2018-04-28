a523a13377ca8f4b6e9719620dc1ec510f0d2a90
John Palmer <johnpalmer.au@gmail.com>
2018-04-28 21:06:20 +1000

    Test test project for travis

--- GribTests/Library.fs
+++ GribTests/Library.fs
@@ -1,8 +1,11 @@
 namespace GribTests
+
+namespace GribTests
 open FsCheck.Xunit
 module Core =
+    open Xunit
 
     [<Property>]
     let ``Simple passing test``(xs:list<int>) =
-      false
+      true
       
