0f8b6e0969f272c21022849e9c2ead9d1b469477
John Palmer <johnpalmer.au@gmail.com>
2018-04-29 21:25:57 +1000

    An actual test for the real code

--- GribTests/Library.fs
+++ GribTests/Library.fs
@@ -1,22 +1,11 @@
 namespace GribTests
 open FsCheck.Xunit
+open Xunit
 module Core =
-    open Xunit
+    [<Property>]
+    let testlen_simple arg = 
+        if arg |> Array.length < 3 then
+            Core.getlen arg = 0
+        else
+            Core.getlen arg > 0
 
-    [<Property>]
-    let ``Simple passing test``(xs:list<int>) =
-      true
-      
-    [<Property>]
-    let test (i:int) =
-        GribViewerCore.Say.testfn i
-
-    [<Xunit.FactAttribute>]
-    let test3 () =
-        GribViewerCore.Say.testytype().TestFunc()
-    [<Xunit.FactAttribute>]
-    let test4 () =
-        true
-    [<Property>]
-    let test5 arg = 
-        Core.getlen arg >= 0
