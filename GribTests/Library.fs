645f7fd4fba5eeb21eba0805bbd2f68056c08eb8
John Palmer <johnpalmer.au@gmail.com>
2018-05-05 16:49:37 +1000

    Add some additional tests

--- GribTests/Library.fs
+++ GribTests/Library.fs
@@ -8,4 +8,10 @@ module Core =
             Core.getlen arg = 0
         else
             Core.getlen arg > 0
+    [<Property>]
+    let ``readLat = readlon`` a b c =
+        Core.readlat a b c = Core.readlon a b c
+    [<Property>]
+    let ``readLatfine = readlonfine`` a b  =
+        Core.readlatFine a b  = Core.readlonFine a b
 
